8e02360ee1b7c0ba0e9bac80f4072d998b5e530b
robilkot <timur.robilko@gmail.com>
2022-12-30 02:38:19 +0300

    rewrite .ino using registers control, optimize

--- 81-717.5_pult_arduino/81-717.5_pult_arduino.ino
+++ 81-717.5_pult_arduino/81-717.5_pult_arduino.ino
@@ -1,34 +1,37 @@
-constexpr short latchPin = 11; //Пин подключен к ST_CP входу 74HC595
-constexpr short clockPin = 12; //Пин подключен к SH_CP входу 74HC595
-constexpr short dataPin = 10; //Пин подключен к DS входу 74HC595
-
+/*
+11 latch ST_CP вход 74HC595
+12 clock SH_CP вход 74HC595
+10 data DS вход 74HC595
+*/
 constexpr short totalRegisters = 8; // Общее число подключенных регистров
 
 void setup()
 {
-  pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT); 
-  pinMode(clockPin, OUTPUT);
+  DDRB |= B00011100; // set 10, 11, 12 to output
   Serial.begin(9600);
+  //registersWrite("1111111100000000010101010000001000000001000000010000000111111111"); // test string
 }
 
 void loop()
 {
-  if (Serial.available()) {
-    //registersWrite("1111111100000000000000010000000000000001000000010000000000000000");
-    registersWrite(Serial.readString());
-  }
+  while(1) if (Serial.available()) registersWrite(Serial.readString());
 }
 
 void registersWrite(String toSend)
 {   
-  digitalWrite(latchPin, LOW); 
+  PORTB &= ~B00001000; // latch LOW
 
   for(short currentRegisterNumber = 0; currentRegisterNumber < totalRegisters; currentRegisterNumber++) {
-    byte currentRegisterByte = B00000000;
-      for(short k = 0; k < 8; k++) bitWrite(currentRegisterByte, k, toSend[currentRegisterNumber*8 + k]-'0');
-      shiftOut(dataPin, clockPin, MSBFIRST, currentRegisterByte);     
+    byte currentRegisterByte = 0;
+      for(uint8_t k = 0; k < 8; k++)
+        currentRegisterByte |= toSend[currentRegisterNumber*8 + k] - '0' << k;
+
+      for (uint8_t k = 0; k < 8; k++, currentRegisterByte <<= 1)  {
+        currentRegisterByte & 128 ? PORTB |= 1 << 2 : PORTB &= ~(1 << 2);
+        PORTB |= B00010000; // clock HIGH
+        PORTB &= ~B00010000; // clock LOW
+      }
   }
 
-  digitalWrite(latchPin, HIGH);
+  PORTB |= B00001000; // latch HIGH
 }
