58b96123ba20d5e37aa21b9cc827341d9e52c335
robilkot <timur.robilko@gmail.com>
2022-12-31 13:19:31 +0300

    Оптимизация и рефакторинг кода микроконтроллера, дополнение таблицы соответствия

--- 81-717.5_pult_arduino/81-717.5_pult_arduino.ino
+++ 81-717.5_pult_arduino/81-717.5_pult_arduino.ino
@@ -1,37 +1,25 @@
-/*
-11 latch ST_CP вход 74HC595
-12 clock SH_CP вход 74HC595
-10 data DS вход 74HC595
-*/
-constexpr short totalRegisters = 8; // Общее число подключенных регистров
-
 void setup()
 {
-  DDRB |= B00011100; // set 10, 11, 12 to output
+  DDRB |= B11111111; // set 8-13 to output
   Serial.begin(9600);
-  //registersWrite("1111111100000000010101010000001000000001000000010000000111111111"); // test string
+  //registersWrite("1111111100000000010101010000001000000001000000010000000111111111", 8, 8, 9, 10);
 }
 
 void loop()
 {
-  while(1) if (Serial.available()) registersWrite(Serial.readString());
+ while(1) if (Serial.available()) registersWrite(Serial.readString(), 8, 8, 9, 10);
 }
 
-void registersWrite(String toSend)
+void registersWrite(String toSend, uint8_t totalRegisters, uint8_t data, uint8_t latch, uint8_t clock)
 {   
-  PORTB &= ~B00001000; // latch LOW
+  // todo: make support for both PORTB and PORTD
+  PORTB &= ~(1 << latch - 8); // latch LOW
 
-  for(short currentRegisterNumber = 0; currentRegisterNumber < totalRegisters; currentRegisterNumber++) {
-    byte currentRegisterByte = 0;
-      for(uint8_t k = 0; k < 8; k++)
-        currentRegisterByte |= toSend[currentRegisterNumber*8 + k] - '0' << k;
-
-      for (uint8_t k = 0; k < 8; k++, currentRegisterByte <<= 1)  {
-        currentRegisterByte & 128 ? PORTB |= 1 << 2 : PORTB &= ~(1 << 2);
-        PORTB |= B00010000; // clock HIGH
-        PORTB &= ~B00010000; // clock LOW
-      }
+  for (uint8_t k = 0; k < 8 * totalRegisters; k++)  {
+    toSend[k] - '0' ? PORTB |= (1 << data - 8) : PORTB &= ~(1 << data - 8); // write 1 or 0 to corresponding bit
+    PORTB |= (1 << clock - 8); // clock HIGH
+    PORTB &= ~(1 << clock - 8); // clock LOW
   }
 
-  PORTB |= B00001000; // latch HIGH
+  PORTB |= (1 << latch - 8); // latch HIGH
 }
