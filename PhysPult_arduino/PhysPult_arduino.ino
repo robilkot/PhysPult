8104f5d3979e60685ec62a3c004eca223a5e6b34
robilkot <timur.robilko@gmail.com>
2023-01-03 23:35:42 +0300

    Прошивка теперь позволяет подключать регистры к цифровым пинам 1-7

---
+++ PhysPult_arduino/PhysPult_arduino.ino
@@ -0,0 +1,59 @@
+void setup()
+{
+  DDRB |= B11111111; // set 8-13 to output
+  DDRD |= B11111110; // set 1-7 to output
+  Serial.begin(9600);
+  registersWrite("1111111100000000010101010000001000000001000000010000000111111111", 8, 6, 9, 10);
+}
+
+void loop()
+{
+ if (Serial.available()) registersWrite(Serial.readString(), 8, 6, 9, 10);
+  //Serial.println(registersRead(1, 8, 10, 9));
+}
+
+void registersWrite(String toSend, uint8_t totalRegisters, uint8_t data, uint8_t latch, uint8_t clock)
+{   
+  volatile uint8_t *DADDR = data > 7 ? &PORTB : &PORTD,
+                   *LADDR = latch > 7 ? &PORTB : &PORTD,
+                   *CADDR = clock > 7 ? &PORTB : &PORTD;
+
+  bool Dshift = DADDR == &PORTB,
+       Lshift = LADDR == &PORTB,
+       Cshift = CADDR == &PORTB;
+
+  *LADDR &= ~(1 << latch - 8 * Lshift); // latch LOW
+
+  for (uint8_t i = 0; i < 8 * totalRegisters; i++)  {
+    toSend[i] - '0' ? *DADDR |= 1 << data - 8 * Dshift : *DADDR &= ~(1 << data - 8 * Dshift); // write 1 or 0 to corresponding bit
+    *CADDR |= 1 << clock - 8 * Cshift; // clock HIGH
+    *CADDR &= ~(1 << clock - 8 * Cshift); // clock LOW
+  }
+
+  *LADDR |= 1 << latch - 8 * Lshift; // latch HIGH
+}
+
+String registersRead(uint8_t totalRegisters, uint8_t data, uint8_t latch, uint8_t clock)
+{
+  volatile uint8_t *DADDR = data > 7 ? &PINB : &PIND,
+                   *LADDR = latch > 7 ? &PORTB : &PORTD,
+                   *CADDR = clock > 7 ? &PORTB : &PORTD;
+
+  bool Dshift = DADDR == &PINB,
+       Lshift = LADDR == &PORTB,
+       Cshift = CADDR == &PORTB;
+
+  String ret;
+
+  *LADDR &= ~(1 << latch - 8 * Lshift); // latch LOW
+  *LADDR |= 1 << latch - 8 * Lshift; // latch HIGH
+
+  for (uint8_t i = 0; i < totalRegisters * 8; i++) {
+    ret += *DADDR & (1 << data - 8 * Dshift) == 1 << data - 8 * Dshift;
+  
+    *CADDR |= 1 << clock - 8 * Cshift; // clock HIGH
+    *CADDR &= ~(1 << clock - 8 * Cshift); // clock LOW
+  }
+  
+  return ret;
+}
